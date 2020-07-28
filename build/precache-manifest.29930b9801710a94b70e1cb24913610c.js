0cd66fa515f3b7d9d7e0422aa5f802ac27a152d3
yj_gu <yj_gu@ctrip.com>
2020-07-28 16:48:59 +0800

    add build/

---
+++ build/precache-manifest.29930b9801710a94b70e1cb24913610c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f6ba7acb182778f8f10f27af64d6bee",
+    "url": "/rxjs-animation-demo/build/index.html"
+  },
+  {
+    "revision": "2a51f210631ba8dc192e",
+    "url": "/rxjs-animation-demo/build/static/js/2.73413834.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/rxjs-animation-demo/build/static/js/2.73413834.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bde78d19b046aae89aea",
+    "url": "/rxjs-animation-demo/build/static/js/main.d74d4939.chunk.js"
+  },
+  {
+    "revision": "768b73c35498517cdae3",
+    "url": "/rxjs-animation-demo/build/static/js/runtime-main.ac06c69a.js"
+  }
+]);
